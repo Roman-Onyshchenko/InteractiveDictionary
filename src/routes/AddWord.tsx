af96130ec50aec743d9464827def64f0f3e4da86
Roman-Onyshchenko <roman.onishencko@yahoo.com>
2023-07-29 20:14:19 +0300

    setup zustand state management

--- src/routes/AddWord.tsx
+++ src/routes/AddWord.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button, Text, View } from 'react-native';
+import { useDumbStore } from '../store';
 
 export default function AddWordScreen({ navigation }: { navigation: any }) {
+  const dumbText = useDumbStore(state => state.dumbText);
   return (
     <>
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Add Word Screen</Text>
+        <Text>Add Word Screen and {dumbText}</Text>
         <Button title="Go to Learning" onPress={() => navigation.navigate('Learning')} />
         <Button title="Go to WordsList" onPress={() => navigation.navigate('WordsList')} />
         <Button title="Go to Statistics" onPress={() => navigation.navigate('Statistics')} />
